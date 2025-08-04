08fce1e25d098f3aaa552f2934e6c7c682ed4216
harun-alrosyid <alrosyid07@gmail.com>
2025-08-04 15:05:55 +0700

    feat: create tree-menu

---
+++ components/ui/tree-menu.tsx
@@ -0,0 +1,45 @@
+import { ChevronRight, File, Folder } from 'lucide-react';
+
+import { Collapsible, CollapsibleContent, CollapsibleTrigger } from './collapsible';
+import { SidebarMenuButton, SidebarMenuItem, SidebarMenuSub } from './sidebar';
+
+function TreeMenu({ item }: { item: string | any[] }) {
+  const [name, ...items] = Array.isArray(item) ? item : [item];
+  if (!items.length) {
+    return (
+      <SidebarMenuButton
+        isActive={name === "button.tsx"}
+        className="data-[active=true]:bg-transparent"
+      >
+        <File />
+        {name}
+      </SidebarMenuButton>
+    );
+  }
+
+  return (
+    <SidebarMenuItem>
+      <Collapsible
+        className="group/collapsible [&[data-state=open]>button>svg:first-child]:rotate-90"
+        defaultOpen={name === "components" || name === "ui"}
+      >
+        <CollapsibleTrigger asChild>
+          <SidebarMenuButton>
+            <ChevronRight className="transition-transform" />
+            <Folder />
+            {name}
+          </SidebarMenuButton>
+        </CollapsibleTrigger>
+        <CollapsibleContent>
+          <SidebarMenuSub>
+            {items.map((subItem, index) => (
+              <TreeMenu key={index} item={subItem} />
+            ))}
+          </SidebarMenuSub>
+        </CollapsibleContent>
+      </Collapsible>
+    </SidebarMenuItem>
+  );
+}
+
+export default TreeMenu
