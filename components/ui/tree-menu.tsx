c4f16589d2b3354481b639d58dc19b1b2617f81f
harun-alrosyid <alrosyid07@gmail.com>
2025-08-04 16:25:17 +0700

    feat: dropdown item

--- components/ui/tree-menu.tsx
+++ components/ui/tree-menu.tsx
@@ -1,9 +1,16 @@
-import { ChevronRight, File, Folder } from 'lucide-react';
+import { ChevronRight, File, Folder, MoreHorizontal } from 'lucide-react';
 
 import { Collapsible, CollapsibleContent, CollapsibleTrigger } from './collapsible';
+import {
+    DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger
+} from './dropdown-menu';
 import { SidebarMenuButton, SidebarMenuItem, SidebarMenuSub } from './sidebar';
 
-function TreeMenu({ item }: { item: string | any[] }) {
+function TreeMenu({
+  item,
+}: {
+  item: string | Array<string | Array<string | Array<string | Array<string>>>>;
+}) {
   const [name, ...items] = Array.isArray(item) ? item : [item];
   if (!items.length) {
     return (
@@ -11,8 +18,22 @@ function TreeMenu({ item }: { item: string | any[] }) {
         isActive={name === "button.tsx"}
         className="data-[active=true]:bg-transparent"
       >
-        <File />
-        {name}
+        <DropdownMenu>
+          <File />
+          {name}
+          <DropdownMenuTrigger asChild>
+            <MoreHorizontal className="ml-auto" />
+          </DropdownMenuTrigger>
+          <DropdownMenuContent
+            side={"right"}
+            align={"start"}
+            className="min-w-56 rounded-lg"
+          >
+            <DropdownMenuItem asChild>
+              <a href={"item.url"}>{"item"}</a>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </SidebarMenuButton>
     );
   }
@@ -25,13 +46,27 @@ function TreeMenu({ item }: { item: string | any[] }) {
       >
         <CollapsibleTrigger asChild>
           <SidebarMenuButton>
-            <ChevronRight className="transition-transform" />
-            <Folder />
-            {name}
+            <DropdownMenu>
+              <ChevronRight className="transition-transform" />
+              <Folder />
+              {name}
+              <DropdownMenuTrigger asChild>
+                <MoreHorizontal className="ml-auto" />
+              </DropdownMenuTrigger>
+              <DropdownMenuContent
+                side={"right"}
+                align={"start"}
+                className="min-w-56 rounded-lg"
+              >
+                <DropdownMenuItem asChild>
+                  <a href={"item.url"}>{"item"}</a>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
           </SidebarMenuButton>
         </CollapsibleTrigger>
-        <CollapsibleContent>
-          <SidebarMenuSub>
+        <CollapsibleContent className="pr-0 mr-0">
+          <SidebarMenuSub className="pr-0 mr-0">
             {items.map((subItem, index) => (
               <TreeMenu key={index} item={subItem} />
             ))}
@@ -42,4 +77,4 @@ function TreeMenu({ item }: { item: string | any[] }) {
   );
 }
 
-export default TreeMenu
+export default TreeMenu;
