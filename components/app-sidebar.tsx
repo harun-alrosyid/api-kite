1cdc86c9ff441fd37fedc0cfc2fc7cab8f6f52ce
harun-alrosyid <alrosyid07@gmail.com>
2025-08-07 23:28:39 +0700

    feat: progress support collection postman

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -35,7 +35,7 @@ const data = {
   ],
   tree: [
     [
-      "app",
+      "appssss      sssssssssssssssss",
       [
         "api",
         ["hello", ["route.ts"]],
@@ -49,6 +49,7 @@ const data = {
       ["ui", "button.tsx", "card.tsx"],
       "header.tsx",
       "footer.tsx",
+      
     ],
     ["lib", ["util.ts"]],
     ["public", "favicon.ico", "vercel.svg"],
@@ -58,6 +59,320 @@ const data = {
     "tailwind.config.js",
     "package.json",
     "README.md",
+  ],
+};
+
+const datas = {
+  info: {
+    _postman_id: "210be4cf-c1c6-4916-bb45-baee784414b9",
+    name: "fcm-firebase Copy",
+    schema:
+      "https://schema.getpostman.com/json/collection/v2.1.0/collection.json",
+    _exporter_id: "45627368",
+    _collection_link:
+      "https://senyum-mobile.postman.co/workspace/Senyum-Mock-Api~4b0e60fa-cb2f-47bf-a23f-dbc4d78903e2/collection/45627368-210be4cf-c1c6-4916-bb45-baee784414b9?action=share&source=collection_link&creator=45627368",
+  },
+  item: [
+    {
+      name: "New Folder",
+      item: [
+        {
+          name: "send notification",
+          request: {
+            auth: {
+              type: "bearer",
+              bearer: [
+                {
+                  key: "token",
+                  value: "{{vault:firebase-cloud-messaging-api-key}}",
+                  type: "string",
+                },
+              ],
+            },
+            method: "POST",
+            header: [
+              {
+                key: "Authorization",
+                value:
+                  "key = AAAAXsYOzmU:APA91bGUJfzyV77QO3uSFp5aWkb4Pm2TOk-d9xI0ddq0ZfC3-R2Z1drmU4rrlw0HlE50hqb1swmXGxn9n00dvnM7l9J3OqrXxefGXpHCR84xyrPHeX-TsuyQEHL4kRK3AVAGotexSDcw",
+                type: "text",
+                disabled: true,
+              },
+              {
+                key: "Content-Type",
+                value: "application/json",
+                type: "text",
+              },
+            ],
+            body: {
+              mode: "raw",
+              raw: '{\n "to" : "eryqlqHxSEmmfct5Z4CkHK:APA91bFyXrV_bFkc9y4TnYSAboKyYJWqiNJxcjZdphXgubwwRlWs9L2XRiP9jruKtSHDjOBexUOA6A0NWuPVs3jJx3p7Fqg-k1gRJY-s9CheGCha9QYwfrGA_njqhtFbmWf8VzkJjg4M",\n "data" : {\n     "body" : "is Body of Your Notification in Data",\n     "title": " Title in background",\n     "type" : "Value for key_1",\n     "targetId" : "oke4",\n     "applicationId":"",\n     "productId":"simpedesUMi"\n }\n}',
+              options: {
+                raw: {
+                  language: "json",
+                },
+              },
+            },
+            url: {
+              raw: "https://fcm.googleapis.com/v1/projects/ddb-senyum-firebase-dev/messages:send",
+              protocol: "https",
+              host: ["fcm", "googleapis", "com"],
+              path: [
+                "v1",
+                "projects",
+                "ddb-senyum-firebase-dev",
+                "messages:send",
+              ],
+            },
+          },
+          response: [
+            {
+              name: "send notification",
+              originalRequest: {
+                method: "POST",
+                header: [
+                  {
+                    key: "Authorization",
+                    value:
+                      "key = AAAAXsYOzmU:APA91bGUJfzyV77QO3uSFp5aWkb4Pm2TOk-d9xI0ddq0ZfC3-R2Z1drmU4rrlw0HlE50hqb1swmXGxn9n00dvnM7l9J3OqrXxefGXpHCR84xyrPHeX-TsuyQEHL4kRK3AVAGotexSDcw",
+                    type: "text",
+                    disabled: true,
+                  },
+                  {
+                    key: "Content-Type",
+                    value: "application/json",
+                    type: "text",
+                  },
+                ],
+                body: {
+                  mode: "raw",
+                  raw: '{\n "to" : "eryqlqHxSEmmfct5Z4CkHK:APA91bFyXrV_bFkc9y4TnYSAboKyYJWqiNJxcjZdphXgubwwRlWs9L2XRiP9jruKtSHDjOBexUOA6A0NWuPVs3jJx3p7Fqg-k1gRJY-s9CheGCha9QYwfrGA_njqhtFbmWf8VzkJjg4M",\n "data" : {\n     "body" : "is Body of Your Notification in Data",\n     "title": " Title in background",\n     "type" : "Value for key_1",\n     "targetId" : "oke4",\n     "applicationId":"",\n     "productId":"simpedesUMi"\n }\n}',
+                  options: {
+                    raw: {
+                      language: "json",
+                    },
+                  },
+                },
+                url: {
+                  raw: "https://fcm.googleapis.com/v1/projects/ddb-senyum-firebase-dev/messages:send",
+                  protocol: "https",
+                  host: ["fcm", "googleapis", "com"],
+                  path: [
+                    "v1",
+                    "projects",
+                    "ddb-senyum-firebase-dev",
+                    "messages:send",
+                  ],
+                },
+              },
+              status: "Unauthorized",
+              code: 401,
+              _postman_previewlanguage: "",
+              header: [
+                {
+                  key: "WWW-Authenticate",
+                  value:
+                    'Bearer realm="https://accounts.google.com/", error="invalid_token"',
+                },
+                {
+                  key: "Vary",
+                  value: "Origin",
+                },
+                {
+                  key: "Vary",
+                  value: "X-Origin",
+                },
+                {
+                  key: "Vary",
+                  value: "Referer",
+                },
+                {
+                  key: "Content-Type",
+                  value: "application/json; charset=UTF-8",
+                },
+                {
+                  key: "Content-Encoding",
+                  value: "gzip",
+                },
+                {
+                  key: "Date",
+                  value: "Thu, 07 Aug 2025 08:04:20 GMT",
+                },
+                {
+                  key: "Server",
+                  value: "scaffolding on HTTPServer2",
+                },
+                {
+                  key: "X-XSS-Protection",
+                  value: "0",
+                },
+                {
+                  key: "X-Frame-Options",
+                  value: "SAMEORIGIN",
+                },
+                {
+                  key: "X-Content-Type-Options",
+                  value: "nosniff",
+                },
+                {
+                  key: "Alt-Svc",
+                  value: 'h3=":443"; ma=2592000,h3-29=":443"; ma=2592000',
+                },
+                {
+                  key: "Transfer-Encoding",
+                  value: "chunked",
+                },
+              ],
+              cookie: [],
+              body: '{\n    "error": {\n        "code": 401,\n        "message": "Request had invalid authentication credentials. Expected OAuth 2 access token, login cookie or other valid authentication credential. See https://developers.google.com/identity/sign-in/web/devconsole-project.",\n        "status": "UNAUTHENTICATED"\n    }\n}',
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: "New Folder",
+      item: [ {
+      name: "New Request",
+      request: {
+        method: "GET",
+        header: [],
+      },
+      response: [],
+    },]
+    ,
+    },
+    {
+      name: "New Request",
+      request: {
+        method: "GET",
+                 header: [
+              {
+                key: "Authorization",
+                value:
+                  "key = AAAAXsYOzmU:APA91bGUJfzyV77QO3uSFp5aWkb4Pm2TOk-d9xI0ddq0ZfC3-R2Z1drmU4rrlw0HlE50hqb1swmXGxn9n00dvnM7l9J3OqrXxefGXpHCR84xyrPHeX-TsuyQEHL4kRK3AVAGotexSDcw",
+                type: "text",
+                disabled: true,
+              },
+              {
+                key: "Content-Type",
+                value: "application/json",
+                type: "text",
+              },
+            ],
+                    body: {
+              mode: "raw",
+              raw: '{\n "to" : "eryqlqHxSEmmfct5Z4CkHK:APA91bFyXrV_bFkc9y4TnYSAboKyYJWqiNJxcjZdphXgubwwRlWs9L2XRiP9jruKtSHDjOBexUOA6A0NWuPVs3jJx3p7Fqg-k1gRJY-s9CheGCha9QYwfrGA_njqhtFbmWf8VzkJjg4M",\n "data" : {\n     "body" : "is Body of Your Notification in Data",\n     "title": " Title in background",\n     "type" : "Value for key_1",\n     "targetId" : "oke4",\n     "applicationId":"",\n     "productId":"simpedesUMi"\n }\n}',
+              options: {
+                raw: {
+                  language: "json",
+                },
+              },
+            },
+      },
+            response: [
+            {
+              name: "send notification",
+              originalRequest: {
+                method: "POST",
+                header: [
+                  {
+                    key: "Authorization",
+                    value:
+                      "key = AAAAXsYOzmU:APA91bGUJfzyV77QO3uSFp5aWkb4Pm2TOk-d9xI0ddq0ZfC3-R2Z1drmU4rrlw0HlE50hqb1swmXGxn9n00dvnM7l9J3OqrXxefGXpHCR84xyrPHeX-TsuyQEHL4kRK3AVAGotexSDcw",
+                    type: "text",
+                    disabled: true,
+                  },
+                  {
+                    key: "Content-Type",
+                    value: "application/json",
+                    type: "text",
+                  },
+                ],
+                body: {
+                  mode: "raw",
+                  raw: '{\n "to" : "eryqlqHxSEmmfct5Z4CkHK:APA91bFyXrV_bFkc9y4TnYSAboKyYJWqiNJxcjZdphXgubwwRlWs9L2XRiP9jruKtSHDjOBexUOA6A0NWuPVs3jJx3p7Fqg-k1gRJY-s9CheGCha9QYwfrGA_njqhtFbmWf8VzkJjg4M",\n "data" : {\n     "body" : "is Body of Your Notification in Data",\n     "title": " Title in background",\n     "type" : "Value for key_1",\n     "targetId" : "oke4",\n     "applicationId":"",\n     "productId":"simpedesUMi"\n }\n}',
+                  options: {
+                    raw: {
+                      language: "json",
+                    },
+                  },
+                },
+                url: {
+                  raw: "https://fcm.googleapis.com/v1/projects/ddb-senyum-firebase-dev/messages:send",
+                  protocol: "https",
+                  host: ["fcm", "googleapis", "com"],
+                  path: [
+                    "v1",
+                    "projects",
+                    "ddb-senyum-firebase-dev",
+                    "messages:send",
+                  ],
+                },
+              },
+              status: "Unauthorized",
+              code: 401,
+              _postman_previewlanguage: "",
+              header: [
+                {
+                  key: "WWW-Authenticate",
+                  value:
+                    'Bearer realm="https://accounts.google.com/", error="invalid_token"',
+                },
+                {
+                  key: "Vary",
+                  value: "Origin",
+                },
+                {
+                  key: "Vary",
+                  value: "X-Origin",
+                },
+                {
+                  key: "Vary",
+                  value: "Referer",
+                },
+                {
+                  key: "Content-Type",
+                  value: "application/json; charset=UTF-8",
+                },
+                {
+                  key: "Content-Encoding",
+                  value: "gzip",
+                },
+                {
+                  key: "Date",
+                  value: "Thu, 07 Aug 2025 08:04:20 GMT",
+                },
+                {
+                  key: "Server",
+                  value: "scaffolding on HTTPServer2",
+                },
+                {
+                  key: "X-XSS-Protection",
+                  value: "0",
+                },
+                {
+                  key: "X-Frame-Options",
+                  value: "SAMEORIGIN",
+                },
+                {
+                  key: "X-Content-Type-Options",
+                  value: "nosniff",
+                },
+                {
+                  key: "Alt-Svc",
+                  value: 'h3=":443"; ma=2592000,h3-29=":443"; ma=2592000',
+                },
+                {
+                  key: "Transfer-Encoding",
+                  value: "chunked",
+                },
+              ],
+              cookie: [],
+              body: '{\n    "error": {\n        "code": 401,\n        "message": "Request had invalid authentication credentials. Expected OAuth 2 access token, login cookie or other valid authentication credential. See https://developers.google.com/identity/sign-in/web/devconsole-project.",\n        "status": "UNAUTHENTICATED"\n    }\n}',
+            },
+          ],
+    },
   ],
 };
 
@@ -153,9 +468,9 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
           <SidebarGroup className="px-0">
             <SidebarGroupContent>
               <SidebarMenu>
-                {data.tree.map((item, index) => (
-                  <TreeMenu key={index} item={item} />
-                ))}
+               
+                  <TreeMenu item={datas} />
+               
               </SidebarMenu>
               {/* {mails.map((mail) => (
                 <a
--- components/ui/tree-menu.tsx
+++ components/ui/tree-menu.tsx
@@ -1,4 +1,4 @@
-import { ChevronRight, File, Folder, MoreHorizontal } from 'lucide-react';
+import { ChevronRight, Folder, MoreHorizontal } from 'lucide-react';
 
 import { Collapsible, CollapsibleContent, CollapsibleTrigger } from './collapsible';
 import {
@@ -6,23 +6,118 @@ import {
 } from './dropdown-menu';
 import { SidebarMenuButton, SidebarMenuItem, SidebarMenuSub } from './sidebar';
 
-function TreeMenu({
-  item,
-}: {
-  item: string | Array<string | Array<string | Array<string | Array<string>>>>;
-}) {
+interface IInfo {
+  _postman_id: string;
+  name: string;
+  schema: string;
+  _exporter_id: string;
+  _collection_link: string;
+}
+
+interface AuthBearer {
+  key: string;
+  value: string;
+  type: string;
+}
+
+interface Auth {
+  type: string;
+  bearer: AuthBearer[];
+}
+
+interface RequestHeader {
+  key: string;
+  value: string;
+  type?: string;
+  disabled?: boolean;
+}
+
+interface Request {
+  auth?: Auth;
+  method: string;
+  header?: RequestHeader[];
+}
+
+interface Url {
+  raw: string;
+  protocol: string;
+  host: string[];
+  path: string[];
+}
+
+interface OriginalRequestBodyOptions {
+  raw: {
+    language: string;
+  };
+}
+
+interface OriginalRequestBody {
+  mode: string;
+  raw: string;
+  options: OriginalRequestBodyOptions;
+}
+
+interface OriginalRequest {
+  method: string;
+  header?: RequestHeader[];
+  body: OriginalRequestBody;
+  url: Url;
+}
+
+interface ResponseHeader {
+  key: string;
+  value: string;
+  type?: string;
+  disabled?: boolean;
+}
+
+interface Response {
+  name?: string;
+  status?: string;
+  code?: number;
+  _postman_previewlanguage?: string;
+  originalRequest?: OriginalRequest;
+  header?: ResponseHeader[];
+  cookie: never[];
+  body?: string;
+}
+
+interface Item {
+  name: string;
+  request?: Request;
+  header?: RequestHeader[];
+  response?: Response[];
+}
+
+interface CollectionItem {
+  info?: IInfo;
+  item?: Item[];
+}
+
+interface TreeMenuProps {
+  item: CollectionItem ;
+}
+
+function TreeMenu({ item }: TreeMenuProps) {
+  console.log(item);
+
+  
+
+
+
   const [name, ...items] = Array.isArray(item) ? item : [item];
-  if (!items.length) {
+
+  if (!("info" in item) ) {
+    const data : Item = item;
     return (
       <SidebarMenuButton
         isActive={name === "button.tsx"}
-        className="data-[active=true]:bg-transparent"
+        className="data-[active=true]:bg-transparent truncate"
       >
         <DropdownMenu>
-          <File />
-          {name}
+          {`${data.request?.method} ${data.name}`}
           <DropdownMenuTrigger asChild>
-            <MoreHorizontal className="ml-auto" />
+            <MoreHorizontal className=" absolute right-0" />
           </DropdownMenuTrigger>
           <DropdownMenuContent
             side={"right"}
@@ -38,20 +133,76 @@ function TreeMenu({
     );
   }
 
+  if (!("info" in item) ) {
+    const data : Item = item;
+
+    
+    if (!("request" in data) ) {
+    const datas : Item = item;
+    return (
+      <SidebarMenuButton
+        isActive={name === "button.tsx"}
+        className="data-[active=true]:bg-transparent truncate"
+      >
+        <DropdownMenu>
+          <Folder/>
+          {`${datas.name}`}
+          <DropdownMenuTrigger asChild>
+            <MoreHorizontal className=" absolute right-0" />
+          </DropdownMenuTrigger>
+          <DropdownMenuContent
+            side={"right"}
+            align={"start"}
+            className="min-w-56 rounded-lg"
+          >
+            <DropdownMenuItem asChild>
+              <a href={"item.url"}>{"item"}</a>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </SidebarMenuButton>
+    );
+  }
+
+
+    return (
+      <SidebarMenuButton
+        isActive={name === "button.tsx"}
+        className="data-[active=true]:bg-transparent truncate"
+      >
+        <DropdownMenu>
+          {`${data.request?.method} ${data.name}`}
+          <DropdownMenuTrigger asChild>
+            <MoreHorizontal className=" absolute right-0" />
+          </DropdownMenuTrigger>
+          <DropdownMenuContent
+            side={"right"}
+            align={"start"}
+            className="min-w-56 rounded-lg"
+          >
+            <DropdownMenuItem asChild>
+              <a href={"item.url"}>{"item"}</a>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </SidebarMenuButton>
+    );
+  }
+  // console.log(item);
+
   return (
     <SidebarMenuItem>
       <Collapsible
         className="group/collapsible [&[data-state=open]>button>svg:first-child]:rotate-90"
-        defaultOpen={name === "components" || name === "ui"}
+        // defaultOpen={name === "components" || name === "ui"}
       >
         <CollapsibleTrigger asChild>
           <SidebarMenuButton>
             <DropdownMenu>
               <ChevronRight className="transition-transform" />
-              <Folder />
-              {name}
+              {item.info?.name}
               <DropdownMenuTrigger asChild>
-                <MoreHorizontal className="ml-auto" />
+                <MoreHorizontal className="absolute right-0" />
               </DropdownMenuTrigger>
               <DropdownMenuContent
                 side={"right"}
@@ -67,9 +218,10 @@ function TreeMenu({
         </CollapsibleTrigger>
         <CollapsibleContent className="pr-0 mr-0">
           <SidebarMenuSub className="pr-0 mr-0">
-            {items.map((subItem, index) => (
-              <TreeMenu key={index} item={subItem} />
-            ))}
+            {item?.item&& item?.item?.length > 0 &&
+              item.item.map((subItem, index) => (
+                <TreeMenu key={index} item={subItem as CollectionItem} />
+              ))}
           </SidebarMenuSub>
         </CollapsibleContent>
       </Collapsible>
